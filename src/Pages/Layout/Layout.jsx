cdc289deccfced599f38094bb74140bc6051195f
Sk. Razibul Islam <iamrazib@gmail.com>
2024-01-12 18:08:18 +0600

    current time added

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -1,11 +1,22 @@
 import { Outlet } from "react-router-dom"
 import { Navbar } from "../../components/Navbar"
 import { Footer } from "../../components/Footer"
+import { useState } from "react";
 
 export const Layout = () => {
+
+    let time = new Date().toLocaleTimeString();
+    const [ctime, setTime] = useState(time);
+    const UpdateTime = () => {
+      time = new Date().toLocaleTimeString();
+      setTime(time);
+    };
+    setInterval(UpdateTime);
+
   return (
       <div>
-          <Navbar/>
+          <Navbar />
+          <div className="text-right me-4">Local Time: {ctime}</div>
           <main className="h-[100vh] w-[100%]">
               <Outlet/>
           </main>
